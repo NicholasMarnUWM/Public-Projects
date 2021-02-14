db30856d64da8d15c9aee5cdad37b424918eeb15
Nicholas Marn <njmarn@uwm.edu>
2021-02-14 15:25:00 -0600

    Added folder and starting to add Scala content for CS 738.

---
+++ Scala/MarnScalaTutorial.scala
@@ -0,0 +1,9 @@
+// File should end with scala extension or .scala
+
+// First. begin by importing library functions
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
